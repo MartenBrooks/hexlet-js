f547463cc14727f4e1fb9816b629c7a511c0baae
Yuriy Tarasevich <marten.ao.ns@gmail.com>
2023-11-01 22:19:52 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
 import { fn } from "./src/index.js";
 
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
-
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
 const c = fn(1, 10);
 console.log(c);
+const fun = () => {
+    return true;
+}
